07736e80dd9f1f47facfc0a3e8757428cc18d425
luci1981 <mrluci1981@gmail.com>
2019-04-15 21:28:16 +0300

    add formular si raspuns la acesta

--- boardgames/main/urls.py
+++ boardgames/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('formular/', views.formular, name='formular'),
+    path('formular_submit/', views.formular_submit, name='formular_submit'),
 ]
--- boardgames/main/views.py
+++ boardgames/main/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'main/home.html', {'message':'Hi, there!'})
 
+def formular(request):
+    return render(request, "main/formular.html")
+
+def formular_submit(request):
+    return render(request, "main/raspunsformular.html",
+                  {'name':request.POST['nume'],'prenume':request.POST['prenume'],
+                   'trimite':request.POST['trimite']})
