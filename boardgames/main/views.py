53fcc18b1637e7c5574cadbcedd65caa0cf10cd5
luci1981 <mrluci1981@gmail.com>
2019-04-15 20:10:57 +0300

    3

--- boardgames/main/views.py
+++ boardgames/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/home.html', {'message':'Salut!!!'})
+    return render(request, 'main/home.html', {'message':'Hi, there!'})
