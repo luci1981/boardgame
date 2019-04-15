508fbbd6e1df4492f74ec1f00fc09301bcef9305
luci1981 <mrluci1981@gmail.com>
2019-04-15 21:51:21 +0300

    add & use form.py

--- boardgames/main/views.py
+++ boardgames/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .forms import PersonForm
+from .models import Person
 
 def home(request):
     return render(request, 'main/home.html', {'message':'Hi, there!'})
@@ -11,3 +12,22 @@ def formular_submit(request):
     return render(request, "main/raspunsformular.html",
                   {'name':request.POST['nume'],'prenume':request.POST['prenume'],
                    'trimite':request.POST['trimite']})
+
+
+def person(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            person = Person()
+            person.nume = cd['nume']
+            person.mail = cd['email']
+            person.phonenumber = cd['phonenumber']
+            person.save()
+            form = PersonForm()
+        else:
+            print("Form is not valid")
+    else:
+        form = PersonForm()
+
+    return render(request, 'main/contact_form.html', {'form': form})
